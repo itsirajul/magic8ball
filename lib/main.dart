b8b85f4b1a64dd6400cca6e95f85dcdc7240d2a6
itsirajul <shirajulislamparvez@gmail.com>
2022-03-12 21:41:38 +0600

    Color Change

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('8 Ring Ball',style: TextStyle(color: Colors.black),),
-          backgroundColor: Colors.redAccent,
+          backgroundColor: Colors.greenAccent,
         ),
       )
       ),
