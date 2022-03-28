f910d9cbdf0e2bbf19998787785f50b772caebf8
itsirajul <shirajulislamparvez@gmail.com>
2022-03-28 22:44:05 +0600

    regular commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main(){
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('8 Ring',style: TextStyle(color: Colors.deepOrange),),
+          title: const Text('8 Ring',style: TextStyle(color: Colors.white10),),
           backgroundColor: Colors.redAccent,
         ),
       )
