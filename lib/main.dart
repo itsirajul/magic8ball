9b460f3724e6644d2b29521e82ea0bef6449d79a
itsirajul <shirajulislamparvez@gmail.com>
2022-04-15 20:27:32 +0600

    Ammend Command

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main(){
       home: Scaffold(
         appBar: AppBar(
           title: const Text('8 Ring'),
-          backgroundColor: Colors.yellowAccent
+          backgroundColor: Colors.orangeAccent
         ),
       )
     ),
