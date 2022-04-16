090103c1b1ef794cf79f36e8c65fab09d853c8f1
itsirajul <shirajulislamparvez@gmail.com>
2022-04-16 20:27:32 +0600

    Final Deploy

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main(){
       home: Scaffold(
         appBar: AppBar(
           title: const Text('8 Ring'),
-          backgroundColor: Colors.orangeAccent
+          backgroundColor: Colors.blueAccent
         ),
       )
     ),
